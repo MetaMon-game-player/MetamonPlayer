1ca116ecca89e946d6420473d62fa94462cd08ae
Afaelin <thelen.patrick@hotmail.com>
2022-01-18 22:07:21 +0100

    Fix: No more page is required to get the entire Metamon list, the loop is now obsolete.

--- metamon_play.py
+++ metamon_play.py
@@ -197,23 +197,19 @@ class MetamonPlayer:
         mtm_stats_df = pd.DataFrame(mtm_stats)
         print(mtm_stats_df)
         self.mtm_stats_df.append(mtm_stats_df)
-
+        
     def get_wallet_properties(self):
         """ Obtain list of metamons on the wallet"""
         data = []
-        page = 1
-        while True:
-            payload = {"address": self.address, "page": page, "pageSize": 60}
-            headers = {
-                "accessToken": self.token,
-            }
-            response = post_formdata(payload, WALLET_PROPERTY_LIST, headers)
-            mtms = response.get("data", {}).get("metamonList", [])
-            if len(mtms) > 0:
-                data.extend(mtms)
-                page += 1
-            else:
-                break
+        payload = {"address": self.address}
+        headers = {
+            "accessToken": self.token,
+        }
+        response = post_formdata(payload, WALLET_PROPERTY_LIST, headers)
+        mtms = response.get("data", {}).get("metamonList", [])
+        if len(mtms) > 0:
+            data.extend(mtms)
+            data = sorted(data, key=lambda metamon: metamon['id'])
         return data
 
     def list_monsters(self):
@@ -233,7 +229,6 @@ class MetamonPlayer:
         mtm_stats_file_name = f"{w_name}_stats.tsv"
         self.init_token()
 
-        self.get_wallet_properties()
         monsters = self.list_monsters()
         wallet_monsters = self.get_wallet_properties()
         print(f"Monsters total: {len(wallet_monsters)}")
