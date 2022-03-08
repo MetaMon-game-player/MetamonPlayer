7ffc0bbae72e2ca8578b329ba555c335858d191d
Thuc Nguyen <thucnguyenit35@gmail.com>
2022-03-08 11:45:22 +0700

    Update metamon_play.py

--- metamon_play.py
+++ metamon_play.py
@@ -19,8 +19,11 @@ LIST_BATTLER_URL = f"{BASE_URL}/getBattelObjects"
 WALLET_PROPERTY_LIST = f"{BASE_URL}/getWalletPropertyList"
 LVL_UP_URL = f"{BASE_URL}/updateMonster"
 MINT_EGG_URL = f"{BASE_URL}/composeMonsterEgg"
+<<<<<<< HEAD
 OPEN_EGG_URL = f"{BASE_URL}/openMonsterEgg"
 CHECK_BAG_URL = f"{BASE_URL}/checkBag"
+=======
+>>>>>>> parent of 44ecbff (add open eggs function)
 
 
 def datetime_now():
@@ -328,24 +331,6 @@ class MetamonPlayer:
 
         print(f"Minted Eggs Total: {total_egg}")
 
-    def open_eggs(self, egg_count = 0):
-        if self.token is None or self.token == '':
-            self.init_token()
-
-        headers = {
-            "accessToken": self.token
-        }
-        payload = {"address": self.address}
-
-        for i in range(egg_count):
-            res = post_formdata(payload, OPEN_EGG_URL, headers)
-            code = res.get("code")
-            if code != "SUCCESS":
-                print("Open Eggs Failed: {}".format(res.get("message")))
-                break
-            data = res.get("data")
-            print("Open Eggs Success: {} x {}".format(data.get("amount"), data.get("category")))
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -367,8 +352,11 @@ if __name__ == "__main__":
                                                      "Two files <name>_summary.tsv and <name>_stats.tsv will "
                                                      "be saved in current dir.",
                         action="store_true", default=False)
+<<<<<<< HEAD
     parser.add_argument("-o", "--open-eggs", help="Open the eggs with amount",
                         type=int, default=0)
+=======
+>>>>>>> parent of 44ecbff (add open eggs function)
 
     args = parser.parse_args()
 
@@ -395,4 +383,3 @@ if __name__ == "__main__":
             mtm.battle(w_name=r["name"])
         if args.mint_eggs:
             mtm.mint_eggs()
-        mtm.open_eggs(args.open_eggs)
